0060f1ef58eef782df2c778ae2bd4583642ff311
adnv3k <adnv3k@gmail.com>
2025-07-15 15:59:26 -0700

    setup files for assignment
    remove default content for src files and add prettier and eslint config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
+import react from 'eslint-plugin-react'
+// import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -8,8 +9,16 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     extends: [
       js.configs.recommended,
+      js.configs.recommended.rules, // added for CTD instructions
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
@@ -22,8 +31,15 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'warn',
+      'react/prop-types': 'off',
     },
   },
 ])
